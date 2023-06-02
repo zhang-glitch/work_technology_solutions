202d3ac32a9a867da9e585207ab241dff51e4316
zhang-glitch <669638556@qq.com>
2023-06-02 21:51:39 +0800

    feat 通过tailwindcss 定制化、高可用前台样式处理方案

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // tailwindcss 应用的文件范围
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
